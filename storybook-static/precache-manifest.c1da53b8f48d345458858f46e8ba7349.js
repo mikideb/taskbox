aa1047c6cf6179b1b1e95f2289f4439fe129aaab
Miki Debran <miki@thesashka.com>
2020-10-06 09:41:09 +0300

    taskbox UI

---
+++ storybook-static/precache-manifest.c1da53b8f48d345458858f46e8ba7349.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf40e4022ad2c38c7d361a9d2743ef65",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.1f52fdc2fe2f7ac906ab.bundle.js"
+  },
+  {
+    "url": "runtime~main.1f52fdc2fe2f7ac906ab.bundle.js"
+  },
+  {
+    "revision": "3f3d83713f164f1f5355",
+    "url": "static/css/main.3d37e275.chunk.css"
+  },
+  {
+    "url": "vendors~main.1f52fdc2fe2f7ac906ab.bundle.js"
+  },
+  {
+    "url": "vendors~main.1f52fdc2fe2f7ac906ab.bundle.js.LICENSE.txt"
+  }
+]);
